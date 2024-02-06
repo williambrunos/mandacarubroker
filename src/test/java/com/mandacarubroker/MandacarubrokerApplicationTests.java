9854fd44807df74a4049c2f2969ed970cee40175
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2024-02-05 21:54:56 -0300

    chore: renomeando os nomes dos métodos de testes

--- src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
+++ src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
@@ -17,19 +17,19 @@ class MandacarubrokerApplicationTests {
 	private MockMvc mockMvc;
 
 	@Test
-	void testGetStocks() throws Exception {
+	void shouldGetAllStocks() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/stocks"))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	@Test
-	void testGetStockByIdNotFound() throws Exception {
+	void shouldNotGetStockByInexistentId() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/stocks/{id}", "teste_falha"))
 				.andExpect(MockMvcResultMatchers.status().isNotFound());
 	}
 
 	@Test
-	void testCreateStock() throws Exception {
+	void shouldCreateStock() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.post("/stocks")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"TST4\", \"price\":  45.2}"))
@@ -37,7 +37,7 @@ class MandacarubrokerApplicationTests {
 	}
 
 	@Test
-	void testUpdateStock() throws Exception {
+	void shouldUpdateStockById() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.put("/stocks/{id}", 1)
 						.contentType(MediaType.APPLICATION_JSON)
 						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"TST4\", \"price\":  45}"))
@@ -45,13 +45,13 @@ class MandacarubrokerApplicationTests {
 	}
 
 	@Test
-	void testDeleteStock() throws Exception {
+	void shouldDeleteStockById() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.delete("/stocks/{id}", 1))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 	@Test
-	void testDeleteAllStocks() throws Exception {
+	void shouldNotAllowDeleteAllStocks() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.delete("/stocks"))
 				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
 	}
