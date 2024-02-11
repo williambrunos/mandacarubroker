e2b840005ee0946511904bdee11eb3aa65d98402
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2024-02-11 01:42:19 -0300

    feat: adicionando loggers e corrigindo retorno do método updateStock no código StockService

--- src/main/java/com/mandacarubroker/service/StockService.java
+++ src/main/java/com/mandacarubroker/service/StockService.java
@@ -3,16 +3,21 @@ package com.mandacarubroker.service;
 import com.mandacarubroker.domain.stock.RequestStockDTO;
 import com.mandacarubroker.domain.stock.Stock;
 import com.mandacarubroker.domain.stock.StockRepository;
+
 import jakarta.validation.ValidatorFactory;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Service class for handling operations related to stocks.
@@ -26,49 +31,79 @@ public class StockService {
     private final StockRepository stockRepository;
 
     /**
-     * Constructs a new StockService with the given StockRepository.
-     * @param stockRepo The repository for accessing stock data.
+     * The StockService used by this controller to perform operations on stocks.
      */
-    public StockService(final StockRepository stockRepo) {
-        this.stockRepository = stockRepo;
+    private static final Logger logger = LoggerFactory.getLogger(StockService.class);
+
+    /**
+     * The Validator instance used for validating objects.
+     * This Validator instance is responsible for performing validation checks
+     * on objects, typically instances of DTOs (Data Transfer Objects) or domain
+     * objects, to ensure they meet the specified constraints defined by validation
+     * annotations such as @NotNull, @Size, @Pattern, etc. The Validator is obtained
+     * from the ValidatorFactory, which is responsible for creating Validator instances
+     * based on the validation provider configuration.
+     *
+     * @see jakarta.validation.Validator
+     * @see jakarta.validation.ValidatorFactory
+     */
+    private final Validator validator;
+
+    /**
+     * Constructs a new StockService with the given StockRepository.
+     *
+     * @param stockRepository The repository for accessing stock data.
+     */
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            this.validator = factory.getValidator();
+        }
     }
 
     /**
      * Retrieves all stocks from the repository.
+     *
      * @return A list of all stocks.
      */
     public List<Stock> getAllStocks() {
+        logger.info("Retrieving all stocks");
         return stockRepository.findAll();
     }
 
     /**
      * Retrieves a stock by its ID.
+     *
      * @param id The ID of the stock to retrieve.
      * @return An Optional containing the stock, or empty if not found.
      */
-    public Optional<Stock> getStockById(final String id) {
+    public Optional<Stock> getStockById(String id) {
+        logger.info("Retrieving stock by ID: {}", id);
         return stockRepository.findById(id);
     }
 
     /**
      * Creates a new stock based on the provided data.
+     *
      * @param data The data for creating the new stock.
      * @return The newly created stock.
      */
-    public Stock createStock(final RequestStockDTO data) {
-        Stock newStock = new Stock(data);
+    public Stock createStock(RequestStockDTO data) {
+        logger.info("Creating new stock");
         validateRequestStockDTO(data);
+        Stock newStock = new Stock(data);
         return stockRepository.save(newStock);
     }
 
     /**
      * Updates an existing stock with the provided ID and data.
-     * @param id The ID of the stock to update.
+     *
+     * @param id           The ID of the stock to update.
      * @param updatedStock The updated data for the stock.
-     * @return An Optional containing the updated stock, or empty if not found.
      */
-    public Optional<Stock> updateStock(final String id, final Stock updatedStock) {
-        return stockRepository.findById(id)
+    public void updateStock(String id, Stock updatedStock) {
+        logger.info("Updating stock with ID: {}", id);
+        stockRepository.findById(id)
                 .map(stock -> {
                     stock.setSymbol(updatedStock.getSymbol());
                     stock.setCompanyName(updatedStock.getCompanyName());
@@ -80,40 +115,41 @@ public class StockService {
 
     /**
      * Deletes a stock with the provided ID.
+     *
      * @param id The ID of the stock to delete.
      */
-    public void deleteStock(final String id) {
+    public void deleteStock(String id) {
+        logger.info("Deleting stock with ID: {}", id);
         stockRepository.deleteById(id);
     }
 
     /**
      * Validates the RequestStockDTO object.
+     *
      * @param data The RequestStockDTO object to validate.
      * @throws ConstraintViolationException If validation fails.
      */
-    public static void validateRequestStockDTO(final RequestStockDTO data) {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
+    public void validateRequestStockDTO(RequestStockDTO data) {
+        logger.info("Validating RequestStockDTO");
         Set<ConstraintViolation<RequestStockDTO>> violations = validator.validate(data);
-
         if (!violations.isEmpty()) {
             StringBuilder errorMessage = new StringBuilder("Validation failed. Details: ");
-
             for (ConstraintViolation<RequestStockDTO> violation : violations) {
                 errorMessage.append(String.format("[%s: %s], ", violation.getPropertyPath(), violation.getMessage()));
             }
-
             errorMessage.delete(errorMessage.length() - 2, errorMessage.length());
-
+            logger.error(errorMessage.toString());
             throw new ConstraintViolationException(errorMessage.toString(), violations);
         }
     }
 
     /**
      * Validates the RequestStockDTO object and creates a new stock if validation passes.
+     *
      * @param data The RequestStockDTO object to validate and create a stock from.
      */
-    public void validateAndCreateStock(final RequestStockDTO data) {
+    public void validateAndCreateStock(RequestStockDTO data) {
+        logger.info("Validating and creating new stock");
         validateRequestStockDTO(data);
         Stock newStock = new Stock(data);
         stockRepository.save(newStock);
