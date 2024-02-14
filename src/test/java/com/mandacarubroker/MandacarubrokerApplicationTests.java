400cb2698724ac4569801b21d248de2530041b1a
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2024-02-14 11:06:42 -0300

    feat: adicionando cenários de testes para verificar valor do preço e checar códigos HTTP retornados pelo controller

--- src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
+++ src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
@@ -1,6 +1,5 @@
 package com.mandacarubroker;
 
-import jakarta.validation.constraints.Null;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,7 +11,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MandacarubrokerApplicationTests {
+class StockControllerTests {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -37,12 +36,82 @@ class MandacarubrokerApplicationTests {
 				.andExpect(MockMvcResultMatchers.status().isCreated());
 	}
 
+    @Test
+    void shouldNotAllowCreateStockWithNonPositivePrice() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/stocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"companyName\": \"Test Stock\", \"symbol\": \"SANB4\", \"price\":  -10}"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void shouldNotAllowCreateStockWithZeroValueOnPrice() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/stocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"companyName\": \"Test Stock\", \"symbol\": \"SANB4\", \"price\":  0}"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
 	@Test
-	void shouldUpdateStockById() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.put("/stocks/{id}", 1)
+	void shouldReturnDataAfterCreate() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/stocks")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"SANB4\", \"price\":  45.2}"))
+				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.companyName").value("Test Stock"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.symbol").value("SANB4"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.price").value(45.2));
+	}
+
+	@Test
+	void shouldReturnSuccessfulCodeAfterGetAllStocks() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/stocks"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	void shouldReturnCreatedCodeAfterCreatedStockData() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/stocks")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"SANB4\", \"price\":  45.2}"))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
+	}
+
+	@Test
+	void shouldReturnNotFoundWhenIdNotFoundOnGetData() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/stocks/{id}", "non_existing_id"))
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
+	}
+
+	@Test
+	void shouldReturnNotAllowedWhenIdNotFoundOnPostData() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/stocks/{id}", "non_existing_id")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"SANB4\", \"price\":  45.2}"))
+				.andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+	}
+
+	@Test
+	void shouldReturnNotFoundWhenIdNotFoundOnPutData() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.put("/stocks/{id}", "non_existing_id")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"TST4\", \"price\":  45}"))
-				.andExpect(MockMvcResultMatchers.status().isOk());
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
+	}
+
+	@Test
+	void shouldReturnNotFoundWhenIdNotFoundOnDeleteData() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.delete("/stocks/{id}", "non_existing_id"))
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
+	}
+
+
+	@Test
+	void shouldUpdateStockById() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.put("/stocks/{id}", "f19197c4-38be-462c-a99f-4d32e91c74f3")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{ \"companyName\": \"Test Stock\", \"symbol\": \"TST4\", \"price\":  45}"))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
 	}
 
 	@Test
@@ -56,7 +125,7 @@ class MandacarubrokerApplicationTests {
 
 	@Test
 	void shouldDeleteStockById() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.delete("/stocks/{id}", 1))
+		mockMvc.perform(MockMvcRequestBuilders.delete("/stocks/{id}", "1b1ab7e8-bf1c-4cbf-98a9-94288e04fb87"))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
