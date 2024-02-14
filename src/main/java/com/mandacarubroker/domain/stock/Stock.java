7ef4e8eb51e96bb46012a5b8405a7c0db42fcd27
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2024-02-14 11:09:35 -0300

    fix: ajustando método construtor para que o preço passado no JSON do create ou update seja o preço real, e não a variação positiva ou negativa do opreço

--- src/main/java/com/mandacarubroker/domain/stock/Stock.java
+++ src/main/java/com/mandacarubroker/domain/stock/Stock.java
@@ -55,7 +55,7 @@ public class Stock {
     public Stock(final RequestStockDTO requestStockDTO) {
         this.symbol = requestStockDTO.symbol();
         this.companyName = requestStockDTO.companyName();
-        this.price = changePrice(requestStockDTO.price(), true);
+        this.price = requestStockDTO.price();
     }
 
     /**
